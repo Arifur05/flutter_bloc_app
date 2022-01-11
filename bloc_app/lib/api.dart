ba72d5bd955ee4f7645f120bd40a206684f5816c
Arifur Rashid <arifrashid06@gmail.com>
2022-01-12 00:02:47 +0600

    http

--- bloc_app/lib/api.dart
+++ bloc_app/lib/api.dart
@@ -1,7 +1,8 @@
 import 'dart:convert';
 
-
-import 'constants.dart';
+import 'package:bloc_app/constants.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'models/news_model.dart';
 
 class Api {
@@ -9,15 +10,15 @@ class Api {
     var client = http.Client();
     var newsmodel;
 
-    try{
-      var response = await client.get(Constants.news_url);
+    try {
+      var response = await get(Uri.parse(Constants.news_url));
 
       if (response.statusCode == 200) {
         var jsonString = response.body;
-        var jsonmap=json.decode(jsonString);
-        newsmodel= NewsModel.fromJson(jsonmap);
-      }}
-    catch(Exception) {
+        var jsonmap = json.decode(jsonString);
+        newsmodel = NewsModel.fromJson(jsonmap);
+      }
+    } catch (Exception) {
       return newsmodel;
     }
     return newsmodel;
